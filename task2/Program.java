394e3bcd1eafa370f08a86dad8549378777d6a2d
Илья Нагачев <Ilya>
2024-03-16 17:13:14 +0300

    first commit

---
+++ task2/Program.java
@@ -0,0 +1,25 @@
+package task2;
+
+import java.util.Arrays;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Worker[] workers = EmployeeFabric.generateEmployees(15);
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
+
+        System.out.println();
+        System.out.println("***");
+        System.out.println();
+
+        Arrays.sort(workers);
+
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
+    }
+
+}
